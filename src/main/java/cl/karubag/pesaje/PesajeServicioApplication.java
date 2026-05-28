f9e5aa8d21451ee5b39d673ef7dbdeb1f6dc6897
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 18:29:56 -0400

    pesaje-servicio

---
+++ src/main/java/cl/karubag/pesaje/PesajeServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.pesaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesajeServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesajeServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/pesaje/PesajeServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.pesaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesajeServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
